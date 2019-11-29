ac330be739090b5f426dc6e36c07650e9fc150d3
wanghuidegit <Administrator@J7J7QOJYULIFAFX>
2019-11-29 15:06:30 +0800

    1

---
+++ rabbitmq-durabledirect-provider/src/test/java/com/bjsxt/QueueTest.java
@@ -0,0 +1,32 @@
+package com.bjsxt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 消息队列测试类
+ * @author Administrator
+ *
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=SpringbootApplication.class)
+public class QueueTest {
+	@Autowired
+	private Sender sender; 
+	/*
+	 * 测试消息队列
+	 */
+	@Test
+	public void test1() throws Exception{
+		int flag = 0;
+		while (true) {
+			flag++;
+			Thread.sleep(2000);
+			this.sender.send("Hello RabbitMQ " + flag);
+		}
+	}
+}
