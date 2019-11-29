b670b74d92f414e1b40697f2cfc2901e21236de4
wanghuidegit <Administrator@J7J7QOJYULIFAFX>
2019-11-29 17:11:42 +0800

    555

--- testgit/src/test/com/Test2.java
+++
@@ -1,7 +0,0 @@
-package test.com;
-
-public class Test2 {
-	public static void main(String[] args) {
-		System.out.println("ÄãºÃ!");
-	}
-}
--- testgit/src/test/com/Test3.java
+++
@@ -1,7 +0,0 @@
-package test.com;
-
-public class Test3 {
-	public static void main(String[] args) {
-		System.out.println("OK ÄãºÃ£¡");
-	}
-}
