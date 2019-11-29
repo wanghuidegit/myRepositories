995a81f257df3a94e986de400197a0986784b8cb
wanghuidegit <Administrator@J7J7QOJYULIFAFX>
2019-11-29 14:35:37 +0800

    2

---
+++ rabbitmq-ack-direct-provider/src/test/java/com/bjsxt/QueueTest.java
@@ -0,0 +1,27 @@
+package com.bjsxt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 消息队列测试类
+ * @author Administrator
+ *
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=SpringbootApplication.class)
+public class QueueTest {
+	@Autowired
+	private Sender sender; 
+	/*
+	 * 测试消息队列
+	 */
+	@Test
+	public void test1() throws Exception{
+			this.sender.send("Hello RabbitMQ");
+	}
+}
